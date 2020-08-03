2012805b23f70b99f70795f7e4d8521c21340ed7
ivorycode <kyo3553@naver.com>
2020-08-03 15:24:04 +0900

    Add: [TripAdvisor] 초기세팅

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -1,25 +1,61 @@
 import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
 
-const GlobalStyles = createGlobalStyle`
+const GlobalStyle = createGlobalStyle`
 
-${reset}
+    a{
+        text-decoration:none;
+        color: black;
+    }
 
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  border :0;
-}
+    * {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
 
-ul, li {
-  list-style : none;
-}
+    html, body, div, span, applet, object, iframe,
+    h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+    a, abbr, acronym, address, big, cite, code,
+    del, dfn, em, img, ins, kbd, q, s, samp,
+    small, strike, strong, sub, sup, tt, var,
+    b, u, i, center,
+    dl, dt, dd, ol, ul, li,
+    fieldset, form, label, legend,
+    table, caption, tbody, tfoot, thead, tr, th, td,
+    article, aside, canvas, details, embed,
+    figure, figcaption, footer, header, hgroup,
+    menu, nav, output, ruby, section, summary,
+    time, mark, audio, video {
+        margin: 0;
+        border: 0;
+        font-size: 100%;
+        font-family: "Arial", sans-serif;
+        vertical-align: baseline;
+    }
 
-a {
-  text-decoration : none;
-}
+    body {
+        line-height: 1.3;
+        font-size: 12px;
+    }
 
+    ol, ul {
+        list-style: none;
+    }
+    
+    button, input, select, textarea {
+        font-size: 100%;
+        margin: 0;
+        vertical-align: baseline;
+    }
+    button, input {
+        padding: 0;
+        border: none;
+        font: inherit;
+        color: inherit;
+        background-color: transparent;
+        cursor: pointer;
+        outline: 0;
+    }
 `;
 
-export default GlobalStyles;
+export default GlobalStyle;
